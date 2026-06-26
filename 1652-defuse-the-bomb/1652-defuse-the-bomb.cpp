b4fa54fc8867650b79fe9f29b2ff8979a810f2fa
Divanshu Sharma <divanshus2107@gmail.com>
2026-06-27 02:48:34 +0530

    Time: 0 ms (100%), Space: 11.6 MB (50.14%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {
-        int n = code.size();
-        vector<int> ans(n, 0);
-
-        if (k == 0) return ans;
-
-        int sum = 0;
-
-        if (k > 0) {
-            // sum of next k elements for index 0
-            for (int i = 1; i <= k; i++)
-                sum += code[i % n];
-
-            for (int i = 0; i < n; i++) {
-                ans[i] = sum;
-
-                sum -= code[(i + 1) % n];
-                sum += code[(i + k + 1) % n];
+        int N = code.size();
+        vector<int> res(N, 0);
+        
+        for (int i = 0; i < N; i++) {
+            if (k > 0) {
+                for (int j = i + 1; j < i + 1 + k; j++) {
+                    res[i] += code[j % N];
+                }
+            }
+            else if (k < 0) {
+                for (int j = i - 1; j > i - 1 - abs(k); j--) {
+                    res[i] += code[((j % N) + N) % N];
+                }
             }
         }
-        else {
-            k = -k;
-
-            // sum of previous k elements for index 0
-            for (int i = 1; i <= k; i++)
-                sum += code[(n - i) % n];
-
-            for (int i = 0; i < n; i++) {
-                ans[i] = sum;
-
-                sum -= code[(i - k + n) % n];
-                sum += code[i];
-            }
-        }
-
-        return ans;
+        
+        return res;
     }
 };
